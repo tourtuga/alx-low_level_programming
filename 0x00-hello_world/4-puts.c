3cd7071d9ed29bf27322edc54f45f6c115e6133e
tourtuga <youssefamoken159@gmail.com>
2023-07-15 05:42:07 -0700

    first project

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main (void){
+/*
+ * main intary of code start point
+ * this print by function puts
+ * always return (0)
+that's mean success*/
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0) ;
+	return (0);
 }
