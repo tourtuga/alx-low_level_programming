283371da125b56ddb62369ac40b280226473da09
tourtuga <youssefamoken159@gmail.com>
2023-07-15 06:02:36 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: A C program the print by printf function
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
