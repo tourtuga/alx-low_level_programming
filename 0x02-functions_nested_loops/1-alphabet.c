c4c0998aed6e22f5e1edf581d32ca4b3df740f44
tourtuga <youssefamoken159@gmail.com>
2023-07-18 17:55:49 -0700

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 int _putchar(char ch);
+void print_alphabet(void);
 
 
 #endif
