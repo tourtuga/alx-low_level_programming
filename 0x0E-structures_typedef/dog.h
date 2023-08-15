156da77c2a5c1f927e0a954811fd6d3e4c6fbe95
tourtuga <youssefamoken159@gmail.com>
2023-08-14 19:30:41 -0700

    sec

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stdio.h>
 /**
  * struct dog - a dog's basic info
  * @name: First member
@@ -16,4 +17,6 @@ struct dog
 
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif
